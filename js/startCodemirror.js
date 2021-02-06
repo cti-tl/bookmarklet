964f916b44d0198f48e95841c5b9e8f86e593964
cti1650 <cti1650@gmail.com>
2021-02-06 22:05:23 +0900

    #1 初期操作

--- js/startCodemirror.js
+++ js/startCodemirror.js
@@ -15,6 +15,9 @@ let ByNames = (name, index = 0) => d.getElementsByName(name)[index];
 let q = (selector, index = 0) => d.querySelectorAll(selector)[index];
 alert('test');
 })(document);`);
+window.onload = function () {
+  jscompCM();
+};
 editor.on("blur", function () {
   jscompCM();
 });
