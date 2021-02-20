76c6c2200b759d9edf0b0df271d52c8542e8ca2f
cti1650 <cti1650@gmail.com>
2021-02-20 18:50:30 +0900

    #10 addScript

---
+++ sample/addScript.js
@@ -0,0 +1,7 @@
+(function (url) {
+  let d = new Date();
+  let s = document.createElement("script");
+  s.charset = "UTF-8";
+  s.src = url;
+  (document.getElementsByTagName("head")[0] || document.body).appendChild(s);
+})("https://cti-tl.github.io/mklet/script/deepl.js");
