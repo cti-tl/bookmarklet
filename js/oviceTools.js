1bfea236931103950b1e2955b27512643cf68d64
cti1650 <cti1650@gmail.com>
2021-03-11 23:22:02 +0900

    test

---
+++ js/oviceTools.js
@@ -0,0 +1,17 @@
+let oviceTools = {
+  writeBoard: (str = "") => {
+    if (~location.href.indexOf("ovice.in")) {
+      document.querySelector(
+        "div.ve-design iframe"
+      ).contentDocument.body.innerHTML = str;
+    }
+  },
+  readBoard: () => {
+    let req = "";
+    if (~location.href.indexOf("ovice.in")) {
+      req = document.querySelector("div.ve-design iframe").contentDocument.body
+        .innerHTML;
+    }
+    return req;
+  },
+};
