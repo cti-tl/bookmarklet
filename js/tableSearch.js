63cd4156c0b8129ab7642e697b15f600c2913885
cti1650 <cti1650@gmail.com>
2021-03-11 18:29:19 +0900

    Update tableSearch.js

--- js/tableSearch.js
+++ js/tableSearch.js
@@ -1,39 +1,39 @@
-(function(){
-  alert('ok');
-})();
-
 let mklet = {
-test: (f, d, e, a, s1, s2, flg) => {
-  if (~location.href.indexOf('RakWF21')) {
-    f = document.getElementsByName('fr_main')[0].contentWindow;
-    d = f.document;
-    if (d.querySelector('table.RakWFHierarchy tbody')) {
-      ele = document.createElement('tr');
-      ele.innerHTML = `<td><button onClick="(function(){d=document;e=d.querySelectorAll('tr.SeiTR,tr.SeiTR2');a=Array.prototype.slice.call(e);a.forEach(function(item){item.hidden=false;});})()">クリア</button><button onClick="(function(){d=document;e=d.querySelectorAll('tr.SeiTR,tr.SeiTR2');a=Array.prototype.slice.call(e);a.forEach(function(item){item.hidden=item.querySelectorAll('td')[29].innerText!=='on';});})()">完了</button><button onClick="(function(){d=document;e=d.querySelectorAll('tr.SeiTR,tr.SeiTR2');a=Array.prototype.slice.call(e);a.forEach(function(item){item.hidden=item.querySelectorAll('td')[29].innerText==='on';});})()">未完</button><input type="text" value="" onkeyup="(function(e){let val=e.target.value.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
+  test: (f, d, e, a, s1, s2, flg) => {
+    if (~location.href.indexOf('RakWF21')) {
+      f = document.getElementsByName('fr_main')[0].contentWindow;
+      d = f.document;
+      if (d.querySelector('table.RakWFHierarchy tbody')) {
+        ele = document.createElement('tr');
+        ele.innerHTML = `<td><button onClick="(function(){d=document;e=d.querySelectorAll('tr.SeiTR,tr.SeiTR2');a=Array.prototype.slice.call(e);a.forEach(function(item){item.hidden=false;});})()">クリア</button><button onClick="(function(){d=document;e=d.querySelectorAll('tr.SeiTR,tr.SeiTR2');a=Array.prototype.slice.call(e);a.forEach(function(item){item.hidden=item.querySelectorAll('td')[29].innerText!=='on';});})()">完了</button><button onClick="(function(){d=document;e=d.querySelectorAll('tr.SeiTR,tr.SeiTR2');a=Array.prototype.slice.call(e);a.forEach(function(item){item.hidden=item.querySelectorAll('td')[29].innerText==='on';});})()">未完</button><input type="text" value="" onkeyup="(function(e){let val=e.target.value.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
    return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
 }).toLowerCase();d=document;e=d.querySelectorAll('tr.SeiTR,tr.SeiTR2');a=Array.prototype.slice.call(e);a.forEach(function(item){item.hidden=!~item.innerText.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
    return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
 }).toLowerCase().indexOf(val);});})(event)"></input></td>`;
-      d.querySelector('table.RakWFHierarchy tbody').appendChild(ele);
+        d.querySelector('table.RakWFHierarchy tbody').appendChild(ele);
+      }
     }
-  }
-},
-strToAscLower: (str) => {
-  return str.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
-   return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
-}).toLowerCase();
-},
-ok:(str) => {
-  return str;
- console.log(str);
-},
-sample:(url) => {
-  return `
+  },
+  strToAscLower: (str) => {
+    return str.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
+      return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+    }).toLowerCase();
+  },
+  ok: (str) => {
+    return str;
+    console.log(str);
+  },
+  sample: (url) => {
+    return `
 javascript: (function(d, j, s) {
   s = d.createElement('script');
   s.src = j;
   d.body.appendChild(s);
 })(document, '${url}') /*__mklet_title:Linkbklet__*/
 `;
-},
+  },
 }
+
+(function(){
+  alert(mklet.ok('test'));
+})();
