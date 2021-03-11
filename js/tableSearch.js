21d2abff4b7aa527631007766c91e59c86557d82
cti1650 <cti1650@gmail.com>
2021-03-11 12:02:20 +0900

    Update tableSearch.js

--- js/tableSearch.js
+++ js/tableSearch.js
@@ -17,3 +17,23 @@ function(f, d, e, a, s1, s2, flg) {
 function ok(str){
  console.log(str);
 }
+
+export default () => {  
+    let title = document.title;
+    let href = document.location.href;
+    let canonical = get_canonical();
+    console.log('ページタイトル=%s \n href=%s \n カノニカルURL=%s', title, href, canonical);
+    alert('ページタイトル：'+title +"\n現在のURL："+href + "\nカノニカルURL：" + canonical);
+
+    function get_canonical(){
+      let links = document.getElementsByTagName("link");
+      for ( let i in links) {
+        if (links[i].rel) {
+          if (links[i].rel.toLowerCase() == "canonical") {
+            return links[i].href;
+          }
+        }
+      }
+      return "";
+    }
+}
